aefdb0db002248846bdbe0ad944fbaba5cc6efb7
Juan Camilo Cuspián <cuspianhuertas@gmail.com>
2025-01-14 11:54:36 -0500

    Agrega archivos del proyecto

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/SpaceApp/',  // Añade esta línea con el nombre de tu repositorio
+})
